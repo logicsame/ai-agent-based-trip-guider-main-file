338d9bd63e0f4b88d09bb3d6dec282b16788b85c
logicsame <useforprofessional@gmail.com>
2025-03-22 15:54:57 +0600

    location weather added

---
+++ src/tripplanner/spot_searching_page/get_location_weather.py
@@ -0,0 +1,13 @@
+
+
+from fastapi import FastAPI, HTTPException, Query, Depends
+from tripplanner.spot_searching_page import get_weather_data
+
+app = FastAPI()
+
+@app.get("/weather")
+async def get_location_weather(lat: float, lon: float):
+    weather_data = get_weather_data(lat, lon)
+    if weather_data:
+        return weather_data
+    raise HTTPException(status_code=404, detail="Weather data unavailable")
